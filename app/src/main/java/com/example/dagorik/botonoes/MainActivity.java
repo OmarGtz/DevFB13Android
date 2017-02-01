cb3f33816539dd924a0abccf57dd2ff012c95df8
Dagorik <dagorik@gmail.com>
2017-02-01 03:40:14 -0600

    second commit

---
+++ app/src/main/java/com/example/dagorik/botonoes/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.dagorik.botonoes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+        private Button bt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bt=(Button) findViewById(R.id.button1);
+        bt.setOnClickListener(this);
+         }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    /*
+    public void hey_button(View view) {
+        switch (view.getId()){
+            case R.id.button1:
+                Toast(get)
+        }
+
+    }
+*/
+
+}
